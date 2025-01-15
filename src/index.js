fb4700f141a8cacc4137a347f380e980cdc2f3e6
josdic1 <joshuadicker@gmail.com>
2025-01-15 16:29:59 -0500

    tasklister completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+//elemental vars
+const text = document.getElementById('new-task-description')
+const ul = document.getElementById('tasks')
+let listHtml;
+let listArray = []
+let textValue;
+
+
+
+document.addEventListener('input', function (e) {
+  textValue = text.value.trim()
+})
+
+document.addEventListener('submit', function (e) {
+  e.preventDefault()
+  listArray = [...listArray, textValue]
+  listHtml = listArray.map(item => `<li>${item}</li>`).join(''); 
+  ul.innerHTML = listHtml
+text.value = ''
+})
+
+
+
+
 });
+
+
